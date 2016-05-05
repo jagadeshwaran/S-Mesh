d8d97b71c698062fb2a11ebf7b4e251451ac1e52
Reach Me some Other Way <jagadeshwaran@users.noreply.github.com>
2016-05-05 16:43:22 -0500

    ADC_C
    c code for fast sampling and averaging

--- ADC_reading_c.c
+++ ADC_reading_c.c
@@ -4,6 +4,24 @@
 #include "snappy.h"
 
 #define REFERENCE_VOLTAGE       ((1 << REFS1) | (1 << REFS0))
+#define numberOfChannels 8
+
+static int16_t  AnalogValues[8][50];
+static int16_t  AnalogValuesOneSample[8];
+static int16_t  counter[numberOfChannels];
+
+void startup()
+{
+	for(int z=0;z<=8;z++)
+	{
+		counter[z] = 0;
+		AnalogValuesOneSample[z] = 0;
+		for(int k=0;k<=130;k++)
+		{
+			AnalogValues[z][k] = 0;
+		}
+	}
+}
 
 int16_t take_sample()
 {
@@ -39,15 +57,54 @@ void enable_adc_and_set_channel(int16_t channel)
 
 int16_t take_averaged_sample(int16_t channel)
 {
+    #define NUM_SAMPLES_PER_AVERAGE1 20
+	int16_t sum = 0;
+	enable_adc_and_set_channel(channel);
+	
+	for ( int i = 0; i < NUM_SAMPLES_PER_AVERAGE1; ++i )
+	{
+		 sum += take_sample();
+	}
+	AnalogValues[channel][counter[channel]] = sum;
+	counter[channel] = counter[channel] + 1;
+	if (counter[channel]==130)
+	{
+		counter[channel]=0;
+	}
+	return sum; // NUM_SAMPLES_PER_AVERAGE;
+	//return AnalogValues[channel][0]; // NUM_SAMPLES_PER_AVERAGE;
+}
+
+void take_averaged_sample_all_channels(int16_t channel)
+{
     #define NUM_SAMPLES_PER_AVERAGE 20
-    int16_t sum = 0;
-    
-    enable_adc_and_set_channel(channel);
-    
-    for ( int i = 0; i < NUM_SAMPLES_PER_AVERAGE; ++i )
+	for(int j=0; j<8; j++)
+	{
+		int16_t sum1 = 0;
+		enable_adc_and_set_channel(j);
+		
+		for ( int l = 0; l < NUM_SAMPLES_PER_AVERAGE; ++l )
+		{
+			 sum1 += take_sample();
+		}
+		
+		AnalogValuesOneSample[j] = sum1;
+	}
+	
+	//return AnalogValuesOneSample; // NUM_SAMPLES_PER_AVERAGE;
+}
+
+int16_t GetADCSanmpledValues(int16_t channel, int16_t sampleNo)
+{
+	return AnalogValues[channel][sampleNo];
+}
+
+void ReadADCdatachunks(uint8_t *s, int16_t count)
+{
+    int16_t k;
+    for(k = 0; k < count; k++)
     {
-        sum += take_sample();
+        s[k] =  's';
     }
-       
-    return sum / NUM_SAMPLES_PER_AVERAGE;
+SET_STR_LEN(s, count);
 }
